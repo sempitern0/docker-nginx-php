45d7c6a9024550d941212b6f79aaebb96f1227d2
sempitern0 <danieldigitalspace@proton.me>
2026-09-15 13:25:09 +0100

    useless commit ever

--- sites/app/public/index.php
+++ sites/app/public/index.php
@@ -11,7 +11,7 @@ require_once dirname(__DIR__) . '/config/bootstrap.php';
 <html lang="<?= e(config('app.locale', 'es')) ?>">
 
 <head>
-    <meta charset="utf-8">
+    <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="APP">
     <title><?= e(config('app.name', 'MYSITE')) ?></title>
